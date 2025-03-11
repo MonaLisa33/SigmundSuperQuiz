34bc7d97d3a025d704a5758a3640202417f26d57
mona <mt221073@fhstp.ac.at>
2025-03-11 12:06:37 +0100

    data layer erstversion

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Quiz } from './Quiz';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
+  public currentQuiz: Quiz = { id: '', quizName: 'newQuiz', questions: [] };
 
-  constructor() { }
+  constructor() {
+    this.currentQuiz.questions.push({
+      id: '1',
+      title: 'What is the capital of France?',
+      a1: 'New York',
+      a2: 'London',
+      a3: 'Paris',
+      a4: 'Dublin',
+      correct: 3,
+    });
+  }
 }
